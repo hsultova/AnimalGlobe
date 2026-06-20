6aa6191e1407894dd77476aff61529243680dc90
Hristomira Sultova <hsultova@users.noreply.github.com>
2026-06-20 22:04:47 +0300

    Project setup. Initialize client and server structure with React and ASP.NET Core web api setup.

---
+++ server/Models/Animal.cs
@@ -0,0 +1,8 @@
+namespace Api.Models;
+
+public class Animal
+{
+    public int Id { get; set; }
+    public string CommonName { get; set; } = "";
+    public string ScientificName { get; set; } = "";
+}
